f33e14f382b2c6b8d5a38ae69aabd02fb1296589
KevinNdirangu <kelvinmwanikindirangu@gmail.com>
2026-04-05 21:29:06 +0300

    Drakaina 1.3.1 - Refined & Split: Standalone Dashboard Moved to Separate Project

--- main.js
+++ main.js
@@ -55,34 +55,3 @@ ipcMain.on('load-data', (event, { file }) => {
         event.reply('load-data-success', { file, data: {} });
     }
 });
-
-// IPC Handler: System Information
-ipcMain.on('get-sys-info', async (event) => {
-    try {
-        const cpu = await si.cpu();
-        const mem = await si.mem();
-        const load = await si.currentLoad();
-        const temp = await si.cpuTemperature();
-        const net = await si.networkStats();
-        
-        event.reply('sys-info-data', {
-            cpu: {
-                brand: cpu.brand,
-                speed: cpu.speed,
-                load: load.currentLoad.toFixed(1),
-                temp: temp.main
-            },
-            mem: {
-                total: (mem.total / 1024 / 1024 / 1024).toFixed(1),
-                used: (mem.used / 1024 / 1024 / 1024).toFixed(1),
-                percent: ((mem.used / mem.total) * 100).toFixed(1)
-            },
-            net: {
-                rx: net[0] ? (net[0].rx_sec / 1024 / 1024).toFixed(2) : "0.00",
-                tx: net[0] ? (net[0].tx_sec / 1024 / 1024).toFixed(2) : "0.00"
-            }
-        });
-    } catch (e) {
-        console.error("System Information Error:", e);
-    }
-});
